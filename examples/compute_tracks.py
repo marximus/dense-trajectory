088ea20f8fe8805a9d9f2a5587e3e9baa596ccaa
Maurice Marx <momomarx@gmail.com>
2017-05-20 06:15:11 -0400

    Update example to correctly add project directory to python path

--- examples/compute_tracks.py
+++ examples/compute_tracks.py
@@ -1,12 +1,11 @@
 import sys
-from os.path import join, dirname
+from os.path import join, dirname, abspath
 
 import numpy as np
 
 # add path so that densetrack can be imported
-sys.path.append(dirname(dirname(__file__)))
+sys.path.append(dirname(dirname(abspath(__file__))))
 import densetrack
-
 
 video = np.load(join(dirname(__file__), "sample_data", "7069-6_roi_0.npy"))
 tracks = densetrack.densetrack(video)
