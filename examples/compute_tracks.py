5be45f19d3b679864f5e5990f5848e057688c548
Maurice Marx <momomarx@gmail.com>
2017-05-20 02:15:27 -0400

    Update example to add path of densetrack.py to python path

--- examples/compute_tracks.py
+++ examples/compute_tracks.py
@@ -1,9 +1,13 @@
+import sys
 from os.path import join, dirname
+
 import numpy as np
 
+# add path so that densetrack can be imported
+sys.path.append(dirname(dirname(__file__)))
 import densetrack
 
+
 video = np.load(join(dirname(__file__), "sample_data", "7069-6_roi_0.npy"))
-print(video.shape)
 tracks = densetrack.densetrack(video)
-print(len(tracks))
+print('Computed {} tracks'.format(len(tracks)))
